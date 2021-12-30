48fa99d86d246b8f8bb77efce6d3017db2c40bba
cyuyanwobuhui <95907085+cyuyanwobuhui@users.noreply.github.com>
2021-12-30 20:15:06 +0800

    Add files via upload

---
+++ 12.25循环结构.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main()
+/*{   
+	int i,s=0;//while循环1加到100 
+i=1;
+while(i<=100)
+{
+	s=s+i;
+	i++;
+}
+printf("%d",s);
+
+
+return 0;
+} */
+
+/*{
+	int i=1,s=0;//do while循环1+100 
+	do
+	{
+	s=s+i;
+	i++;
+	}
+	while(i<=100);
+	printf("%d",s);
+	
+}*/ 
+/* 
+{int i,s;//用for循环求1到10的阶乘 
+for(i=1,s=1;i<11;)
+{
+s=s*i;
+i++;
+}
+printf("%d",s);
+}*/
+
+/*{
+int i=0;
+printf("梅婷傻不傻？\n");
+
+printf("1代表是，2代表不是：\n");
+
+printf("请输入—\n"); 
+
+ scanf("%d/n",&i);
+if(i==1)
+printf("还有药治");
+else
+printf("哈哈哈哈哈恭喜你答对了");
+return 0 ;
+ 
+
+}*/ 
+
+
+
+
+
